3f64be17088c7ef5529466445b22ec3354b026bf
felix2024 <147742264+felix2024@users.noreply.github.com>
2025-09-10 21:05:42 -0700

    SI

---
+++ php/index.php
@@ -0,0 +1,9 @@
+<?php
+    // Esto es un comentario en PHP
+
+    // Declara una variable
+    $saludo = "¡Hola, Mundo desde VS Code!";
+
+    // Imprime el valor de la variable
+    echo $saludo;
+?>
